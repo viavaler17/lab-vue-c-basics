7b39e641dd7048a41d575fa844f8068e3d9ed35c
Valeria Iatchiv <viavaler@amazon.es>
2024-10-12 16:34:11 +0200

    done

---
+++ my-lab-vue/src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+import Footer from './components/Footer.vue';
+import Navbar from './components/Navbar.vue';
+
+// import { ref } from "vue";
+// const calculation = ref(2 + 2);
+
+import { ref } from 'vue';
+
+const name = ref("Valeria");
+
+const message = () => {
+    return "Hello " + name.value + "!";
+}
+
+const changeName = () => {
+  name.value = "Maria"; // Change the name to "Maria"
+}
+const location = ref(true); 
+
+</script>
+
+<template>
+    <div class="message">
+  <!-- <p>{{ calculation }}</p> -->
+  <p>{{ 2 + 2 }}</p>
+    <p> {{ message() }} </p>
+    <button @click="changeName" style="font-weight: 700;">Change Name</button>
+    <br>
+    <p v-if="location">You are in Barcelona!</p>
+    <p v-else>You are in Munich.</p>
+    <Navbar></Navbar>
+    <Footer></Footer>
+    </div>
+</template>
+
+<style scoped>
+.message{
+    background-color: aquamarine;
+    text-align: center;
+    border: 5px solid rgb(53, 1, 1);
+    padding: 100px;
+}
+
+</style>
---
+++ my-lab-vue/src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+import { ref } from 'vue';
+
+const listOfPosts = ref([ 
+{title: 'Where the Crawdads Sing', description: 'Drama', content: 'Book'},
+{title: 'Lessons in Chemistry', description: 'Novel', content: 'Book'},
+{title: 'City of Girls', description: 'Novel', content: 'Book'}
+]);
+</script>
+
+<template>
+    <div>
+        <ul>
+            <li v-for="(item, index) in listOfPosts" :key="index">
+                {{ item.title }} / {{ item.description }} / {{ item.content }}
+            </li>
+        </ul>
+    </div>
+
+</template>
+
+<style scoped>
+</style>
